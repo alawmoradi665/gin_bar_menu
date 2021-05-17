f8e1f85cfedc4238aee3aa229c6b3ddf7f2f0c25
alawmoradi665 <alaw.m@hotmail.com>
2021-05-17 16:16:44 +1000

    first commit

---
+++ index.rb
@@ -0,0 +1,24 @@
+# Gin Bar Menu:
+# Gin Lane Martini $19
+#   -London Dry Gin, Lavender Bitters, Grapefruit Bitters & Tonic
+# Italian Flip $17
+#   -Gin, Campari, Antica Formula, Shaken Well and Ice Block
+# Lightbulb Moment $21
+#   -Tanqueray London Dry Gin, Fresh Mint, Lemon and Peach, Blue Curacao, Rose Essence, Charged with Procecco
+
+# user story:
+# -user should be able to view the menu
+# -user should be able to choose an item from the menu and see its ingredients 
+# -user should be able to specify the quantity of each item 
+# -user should be able to order more than one item
+# -user should be able to exit the app whenever
+# -user should be albe to recieve the final bill with an accurat total amount
+
+# pseudocode
+# 1. display menu and welcome message with directions (choose a drink to see ingredients)
+# 2. get user input
+#     1. if user input a number, display ingredients of the chosen number/drink
+#     2. ask "add to order? yes/no"
+#     3. if user input is yes, add to order and go back to menu 
+#     4. if input is no, go back to menu and continue until exit 
+# 3. if input is exit, display exit message and final bill and exit app 
